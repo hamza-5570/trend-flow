c9802151640f838e118c351d20b1d110689a9a15
hamza-5570 <hafizameerhamza87@gmail.com>
2025-04-14 18:30:19 +0500

    changes

--- model/category.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-const categorySchema = new Schema(
-  {
-    name: { type: String, unique: true, required: true },
-    slug: { type: String, unique: true, required: true },
-  },
-  { timestamps: true }
-);
-export default model('Category', categorySchema);
