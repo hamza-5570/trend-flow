bc90db840a10646552101eeb2c1e571ca8a3d39c
hamza-5570 <hafizameerhamza87@gmail.com>
2025-04-19 23:00:33 +0500

    changes

--- routes/forcastRouter.js
+++ routes/forcastRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 const routes = express.Router();
+import middleware from "../middleware/auth.js";
 import forcastController from "../controller/forcastController.js";
 
 routes.post("/create", forcastController.createForecast);
-routes.get("/all", forcastController.findAll);
+routes.get("/all", middleware.authenticateToken, forcastController.findAll);
 
 export default routes;
