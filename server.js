cdf31ffa4e5082a6ed1273253e238270d5174c2e
hamza-5570 <hafizameerhamza87@gmail.com>
2025-04-20 21:30:04 +0500

    changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import cors from 'cors';
-import routes from './routes/routes.js';
-import connectDB from './config/database.js';
-import multer from 'multer';
-import morgan from 'morgan';
+import cors from "cors";
+import routes from "./routes/routes.js";
+import connectDB from "./config/database.js";
+import multer from "multer";
+import morgan from "morgan";
 
 connectDB();
 
@@ -14,25 +14,26 @@ app.use(express.json({ urlencoded: true }));
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
-      'https://tren-flow.vercel.app',
-      'https://user-api-cp6n.onrender.com',
+      "http://localhost:3001",
+      "https://tren-flow.vercel.app",
+      "https://trend-flow.onrender.com",
+      "https://user-api-cp6n.onrender.com",
     ],
     credentials: true,
   })
 );
 
 // app.use(cors({ origin: 'https://tren-flow.vercel.app' }));
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Ecommerce APIs');
+app.get("/", (req, res) => {
+  res.send("Welcome to Ecommerce APIs");
 });
 
 const PORT = process.env.PORT || 3000;
-app.use('/', routes);
+app.use("/", routes);
 app.listen(PORT, () => {
   console.log(`Server in running and listening on ${PORT}`);
 });
